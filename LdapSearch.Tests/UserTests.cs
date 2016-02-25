d68e3195abc5e8d7a2802625c406aec16f6b5578
Mattias Sämskar <mattias.samskar@gmail.com>
2016-02-25 12:40:26 +0100

    Add user unittests

---
+++ LdapSearch.Tests/UserTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace LdapSearch.Tests
+{
+  public class UserTests
+  {
+    [Fact]
+    public void New_user_memberOf_list_should_not_be_null()
+    {
+      // Arrange
+      var user = new User();
+
+      // Act
+      // Assert
+      Assert.NotNull(user.MemberOf);
+    }
+  }
+}
