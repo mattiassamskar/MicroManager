f2b57039c5cb89f49de68c326fd71c1fbfd5ce71
Mattias Sämskar <mattias.samskar@raysearchlabs.com>
2017-04-21 16:50:52 +0200

    Add ctor to ServiceInfo

--- MicroManager/ServiceInfo.cs
+++ MicroManager/ServiceInfo.cs
@@ -1,7 +1,15 @@
-﻿namespace MicroManager
+﻿using System.Management;
+
+namespace MicroManager
 {
   public class ServiceInfo
   {
+    public ServiceInfo(ManagementBaseObject managementBaseObject)
+    {
+      Name = managementBaseObject["Name"].ToString();
+      State = managementBaseObject["State"].ToString();
+    }
+
     public string Name { get; set; }
 
     public string State { get; set; }
