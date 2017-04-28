84942a432a184c8256bcca5108b07dafb4dfeea6
Mattias Sämskar <mattias.samskar@raysearchlabs.com>
2017-04-28 15:49:15 +0200

    Use overlay icon to indicate service status

--- MicroManager/IconViewModel.cs
+++ MicroManager/IconViewModel.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Media;
 
 namespace MicroManager
 {
   public class IconViewModel : ViewModelBase
   {
-    private string _applicationIcon;
+    private Brush _overlayColor;
 
     public IconViewModel(IObservable<IEnumerable<string>> statesObservable)
     {
@@ -15,26 +16,28 @@ namespace MicroManager
           {
             if (!serviceInfos.Any())
             {
-              ApplicationIcon = "MicroManager.ico";
+              OverlayColor = new SolidColorBrush(Colors.Transparent);
             }
             else
             {
-              ApplicationIcon = serviceInfos.All(si => si == "Running")
-                                  ? "MicroManager-Green.ico"
-                                  : "MicroManager-Red.ico";
+              OverlayColor = serviceInfos.All(si => si == "Running")
+                               ? new SolidColorBrush(Color.FromRgb(34, 177, 76))
+                               : new SolidColorBrush(Color.FromRgb(237, 28, 36));
             }
           });
     }
 
-    public string ApplicationIcon
+    public Brush OverlayColor
     {
       get
       {
-        return _applicationIcon;
+        return _overlayColor;
       }
+
       set
       {
-        _applicationIcon = value;
+        value.Freeze();
+        _overlayColor = value;
         OnPropertyChanged();
       }
     }
